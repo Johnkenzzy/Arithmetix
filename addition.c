9b4480def553c96466278860810c25b926dd8194
Johnkenzzy <johnkenumeh@gmail.com>
2024-07-21 03:46:53 +0100

    implemented the addition arithmetic feature

---
+++ addition.c
@@ -0,0 +1,73 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <stdarg.h>
+
+/**
+* addNums - calculate the sum of a given list of numbers
+*
+* @n: fixed number 
+* @...: array of numbers to be added
+*
+* return: the sum of numbers given
+*/int addNums(int n, int *nums)
+{
+	int i;
+	int sum;
+
+	for (i = 0; i < n; i++)
+	{
+		sum += nums[i];
+	}
+
+	return (sum);
+}
+
+/**
+* num_arr - creates an array with numbers as its elements
+* @: takes no arguments
+* returns: nothing
+*/void num_arr(void)
+{
+	int n;
+	int *arr;
+	int i;
+	int total;
+
+	total = 0;
+	printf("Enter the number of digits you want to sum up: ");
+	scanf("%d", &n);
+
+	if (n < 1)
+	{
+		printf("Number should be grater than Zero!\n");
+		num_arr();
+	}
+
+	arr = malloc(sizeof(int) * n);
+	for (i = 0; i < n; i++)
+	{
+		if (n == 1)
+		{
+			scanf("%d", &arr[i]);
+			total = arr[i];
+			printf("%d", total);
+			return;
+		}
+		else
+		{
+			scanf("%d", &arr[i]);
+			if (arr[i] != arr[n - 1])
+				printf(" + \n");
+		}
+	}
+	total = addNums(n, arr);
+	printf("-----------------\n");
+	printf("Total = %d \n", total);
+	free(arr);
+}
+
+void main()
+{
+	num_arr();
+}
